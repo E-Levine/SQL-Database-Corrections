5819b7824c1fbd12d9f93b50af3407db506728ce
E-Levine <149699489+E-Levine@users.noreply.github.com>
2023-12-01 08:10:35 -0500

    Update DataChecks_2023 12_St Pete.sql
    Added corrections for SampleEventWQ of CRRCRT_20230509_1_0230.

--- DataChecks_2023 12_St Pete.sql
+++ DataChecks_2023 12_St Pete.sql
@@ -7,3 +7,8 @@ go
 UPDATE [dbo].[SampleEvent] 
 SET [TripID] = REPLACE ([TripID], 'CRRCRT_20230413_1','CRRCRT_20230214_1') 
 WHERE [SampleEventID] like 'CRRCRT_20230214_1_%'
+
+--Update 2023/05/09 0230 SampleEventWQ to proofed
+UPDATE [dbo].[SampleEventWQ] 
+SET [DataStatus] = 'Proofed', [DateProofed] = '2023-12-01 00:00:00', [ProofedBy] = 'Erica Levine'
+WHERE [SampleEventID] like 'CRRCRT_20230509_1_0230%'
