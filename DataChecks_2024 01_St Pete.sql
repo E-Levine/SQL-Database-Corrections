a98eccee25d1dbf858c12f43a93d59620fc92005
E-Levine <149699489+E-Levine@users.noreply.github.com>
2024-01-05 07:32:44 -0500

    Update DataChecks_2024 01_St Pete.sql
    Continuing corrections. Updated code for better tracking of completion. Added deployed dates correction for TB 2023/07.

--- DataChecks_2024 01_St Pete.sql
+++ DataChecks_2024 01_St Pete.sql
@@ -8,30 +8,30 @@ update [dbo].[SampleEvent]
 set [SampleEventID] = CONCAT(TripID,'_',(right(replicate('0',4) + cast(FixedLocationID as varchar(4)),4)),'_1')
 where [TripID] like 'TBSDTP_20231117%'
 
-select * into temp_table
-from SampleEvent
-where SampleEventID like 'TBSDTP_202311%' 
-update temp_table
+select * into [temp_table]
+from [SampleEvent]
+where [SampleEventID] like 'TBSDTP_202311%' 
+update [temp_table]
 set [TripID] = 'TBSDTP_20230118_1',
     [SampleEventID] = CONCAT(TripID,'_',(right(replicate('0',4) + cast(FixedLocationID as varchar(4)),4)),'_1')
-insert into SampleEvent
+insert into [SampleEvent]
 select *
-from temp_table
+from [temp_table]
 
-select * into temp_tableb
-from SampleEventWQ
-where SampleEventID like 'TBSDTP_202301%'
-update temp_tableb
+select * into [temp_tableb]
+from [SampleEventWQ]
+where [SampleEventID] like 'TBSDTP_202301%'
+update [temp_tableb]
 set [SampleEventWQID] = REPLACE(SampleEventWQID, '0118','1117'),
 	[SampleEventID] = REPLACE(SampleEventID, '0118', '1117')
-insert into SampleEventWQ
+insert into [SampleEventWQ]
 select *
-from temp_tableb
+from [temp_tableb]
 
 --Update incorrect dates and SampleEventIDs for mixed 01/2023 and 11/2023 samples
 update [dbo].[SedimentTrap]
-set DeployedDate = '2022-12-19' 
-where CupSampleID like 'TB%2301-01%' or CupSampleID like 'TB%2301-07%'
+set [DeployedDate] = '2022-12-19' 
+where [CupSampleID] like 'TB%2301-01%' or CupSampleID like 'TB%2301-07%'
 update [dbo].[SedimentTrap]
 set [SampleEventID] = REPLACE(SampleEventID, '0118','1117') 
 where CupSampleID like 'TB%2311-01%' or CupSampleID like 'TB%2311-07%'
@@ -39,3 +39,8 @@ update [dbo].[SedimentTrap]
 set [SampleEventID] = REPLACE(SampleEventID, '0118','1117'),
 	[DeployedDate] = '2023-10-17'
 where CupSampleID like 'TB%2311-04%' or CupSampleID like 'TB%2311-05%' or CupSampleID like 'TB%2311-06%' or CupSampleID like 'TB%2311-08%'
+
+--Update incorrect deployed dates
+update [dbo].[SedimentTrap]
+set [DeployedDate] = '2023-06-26'
+where [SampleEventID] like 'TBSDTP_202307%' and DeployedDate like '2023-05%'
