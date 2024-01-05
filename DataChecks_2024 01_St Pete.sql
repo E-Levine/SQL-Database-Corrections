960f3290c180dca12c8e10850ad60b723b9078e8
E-Levine <149699489+E-Levine@users.noreply.github.com>
2024-01-05 09:44:11 -0500

    Update DataChecks_2024 01_St Pete.sql
    Code to finish SDTP corrections for LW and CR 01/2023-11/2023.

--- DataChecks_2024 01_St Pete.sql
+++ DataChecks_2024 01_St Pete.sql
@@ -55,14 +55,39 @@ set [Comments] = CONCAT(Comments, ' Data is correct')
 where [CupSampleID] like 'TB%2302-07-3%' or CupSampleID like 'TB%2303-01-1%' or CupSampleID like 'TB%2303-01-2%' or CupSampleID like 'TB%2305-01-1%' or CupSampleID like 'TB%2305-01-2%' or CupSampleID like 'TB%2309-01-1%' or CupSampleID like 'TB%2309-04-2%' or CupSampleID like 'TB%2309-05-1%'
 --End of TBSDTP corrections for 01/2023 - 11/2023.
 
+	
+---LW SDTP Updates
+update [dbo].[SedimentTrap]
+set [Comments] = CONCAT(Comments, case when Comments is null then 'Data is correct' else ', Data is correct' end)
+where [CupSampleID] like 'LW%2311-02-1%' or CupSampleID like 'LW%2311-02-3%' or CupSampleID like 'LW%2311-03-1%' or CupSampleID like 'LW%2311-03-1%' or CupSampleID like 'LW%2311-03-4%'
+--End of LWSDTP corrections for 11/2023.
+
+	
+---CR SDTP Updates
+--Delete incorrect trip date
+DELETE FROM [dbo].[TripInfo]
+WHERE [TripID] like 'CRSDTP_20230712%' 
+--Add required comments for data
+update [dbo].[SedimentTrap]
+set [Comments] = CONCAT(Comments, case when Comments is null then '2 filters used' else ', 2 filters used' end)
+where [CupSampleID] like 'CRE2303-01-1%' or CupSampleID like 'CRE2306-01-2%' or CupSampleID like 'CRW2308-03-2%' or CupSampleID like 'CRW2308-04-1%' 
+update [dbo].[SedimentTrap]
+set [Comments] = CONCAT(Comments, case when Comments is null then 'Data is correct' else ', Data is correct' end)
+where [CupSampleID] like 'CRE2303-01-1%' or CupSampleID like 'CRW2309-03-2%' or CupSampleID like 'CRW2303-04-1%' or CupSampleID like 'CRW2303-04-2%' or CupSampleID like 'CRW2304-04-1%' or CupSampleID like 'CRW2305-04-1%' or CupSampleID like 'CRW2306-04-3%' 
+--End of CRSDTP corrections 01/2023 - 11/2023.
+
 
 IF OBJECT_ID('tempdb..#ValidTrips') IS NOT NULL BEGIN DROP TABLE #ValidTrips; END
 CREATE TABLE #ValidTrips ( TripID VARCHAR(50) );
+
 INSERT INTO #ValidTrips (TripID) SELECT TripID FROM TripInfo WHERE TripDate > '2023-01-01' AND TripDate < '2023-11-30' AND DataStatus = 'Proofed' AND TripID like 'TBSDTP%';
+INSERT INTO #ValidTrips (TripID) SELECT TripID FROM TripInfo WHERE TripDate > '2023-01-01' AND TripDate < '2023-11-30' AND DataStatus = 'Proofed' AND TripID like 'LWSDTP%';
+INSERT INTO #ValidTrips (TripID) SELECT TripID FROM TripInfo WHERE TripDate > '2023-01-01' AND TripDate < '2023-11-30' AND DataStatus = 'Proofed' AND TripID like 'CRSDTP%';
+
 UPDATE TripInfo SET DataStatus = 'Completed', CompletedBy = 'Erica Levine', DateCompleted = '2024-01-05' WHERE TripID IN (SELECT TripID FROM #ValidTrips);
 UPDATE SampleEvent SET DataStatus = 'Completed', CompletedBy = 'Erica Levine', DateCompleted = '2024-01-05' WHERE TripID IN (SELECT TripID FROM #ValidTrips);
 UPDATE SampleEventWQ SET DataStatus = 'Completed', CompletedBy = 'Erica Levine', DateCompleted = '2024-01-05' WHERE SampleEventID IN (SELECT SampleEventID FROM SampleEvent WHERE TripID IN (SELECT TripID FROM #ValidTrips));
-UPDATE SedimentTrap SET DataStatus = 'Completed', CompletedBy = 'Erica Levine', DateCompleted = '%Y-01-05' WHERE SampleEventID IN (SELECT SampleEventID FROM SampleEvent WHERE TripID IN (SELECT TripID FROM #ValidTrips));
+UPDATE SedimentTrap SET DataStatus = 'Completed', CompletedBy = 'Erica Levine', DateCompleted = '2024-01-05' WHERE SampleEventID IN (SELECT SampleEventID FROM SampleEvent WHERE TripID IN (SELECT TripID FROM #ValidTrips));
 INSERT INTO hsdb.TripInfo SELECT * FROM TripInfo WHERE DataStatus = 'Completed';
 DELETE FROM dbo.TripInfo WHERE DataStatus = 'Completed';
 INSERT INTO hsdb.SampleEvent SELECT * FROM SampleEvent WHERE DataStatus = 'Completed';
