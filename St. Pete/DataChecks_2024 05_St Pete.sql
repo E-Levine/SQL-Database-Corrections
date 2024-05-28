83b8b0d197d6e7c564df9ffe89330e4f0f800f53
E-Levine <149699489+E-Levine@users.noreply.github.com>
2024-05-28 10:02:29 -0400

    Update DataChecks_2024 05_St Pete.sql
    Added ode for TB RCRT DOPct updates and TB SRVY updates.

--- St. Pete/DataChecks_2024 05_St Pete.sql
+++ St. Pete/DataChecks_2024 05_St Pete.sql
@@ -24,9 +24,26 @@ UPDATE [dbo].[Recruitment]
 SET [NumBottom] = 0
 WHERE [SampleEventID] like 'TBRCRT_20240326_1_0527%'and [ShellReplicate] = '3' and [ShellPosition] = 5
 
+---Update comments for correct data, DOPct when missing
 UPDATE [dbo].[SampleEventWQ]
 set [Comments] = CONCAT(Comments, case when Comments is null then 'DO correct' else ', DO correct' end)
 where [SampleEventWQID] like 'TBRCRT_20240118_1_0278_1_01'
+
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 121.4 where [SampleEventWQID] like 'TBRCRT_20240118_1_0278_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 114.8 where [SampleEventWQID] like 'TBRCRT_20240118_1_0277_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 105.7 where [SampleEventWQID] like 'TBRCRT_20240118_1_0527_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 105 where [SampleEventWQID] like 'TBRCRT_20240123_1_0528_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 103.4 where [SampleEventWQID] like 'TBRCRT_20240227_1_0527_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 110.7 where [SampleEventWQID] like 'TBRCRT_20240227_1_0528_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 105.6 where [SampleEventWQID] like 'TBRCRT_20240227_1_0529_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 114.8 where [SampleEventWQID] like 'TBRCRT_20240227_1_0529_1_02'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 106.2 where [SampleEventWQID] like 'TBRCRT_20240326_1_0277_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 103.2 where [SampleEventWQID] like 'TBRCRT_20240326_1_0528_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 112.8 where [SampleEventWQID] like 'TBRCRT_20240326_1_0529_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 112 where [SampleEventWQID] like 'TBRCRT_20240326_1_0529_1_02'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 104.9 where [SampleEventWQID] like 'TBRCRT_20240423_1_0527_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 104.6 where [SampleEventWQID] like 'TBRCRT_20240423_1_0529_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 104.1 where [SampleEventWQID] like 'TBRCRT_20240423_1_0529_1_02'
 
 --2023 Survey data
 --TB
@@ -55,10 +72,40 @@ set [DataStatus] = 'Proofed',
 	[ProofedBy] = 'Erica Levine',
 	[Comments] = ''
 where [QuadratID] like 'TBSRVY_20231221_1_0277%' or [QuadratID] like 'TBSRVY_20231221_1_0291%'
- 
+
+--Update SRVY DOPct data
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 100.3 where [SampleEventWQID] like 'TBSRVY_20230627_1_T007_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 107.8 where [SampleEventWQID] like 'TBSRVY_20230627_1_T010_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 146.4 where [SampleEventWQID] like 'TBSRVY_20230822_1_T019_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 129.8 where [SampleEventWQID] like 'TBSRVY_20231101_1_T023_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 117.5 where [SampleEventWQID] like 'TBSRVY_20231101_1_T024_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 137.9 where [SampleEventWQID] like 'TBSRVY_20231101_1_T026_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 104.3 where [SampleEventWQID] like 'TBSRVY_20231221_1_0278_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 206.5 where [SampleEventWQID] like 'TBSRVY_20230822_1_T018_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 112.5 where [SampleEventWQID] like 'TBSRVY_20231221_1_0277_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 115.2 where [SampleEventWQID] like 'TBSRVY_20231221_1_0527_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 104.9 where [SampleEventWQID] like 'TBSRVY_20240226_1_T029_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 143.8 where [SampleEventWQID] like 'TBSRVY_20240226_1_T030_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 144.3 where [SampleEventWQID] like 'TBSRVY_20240226_1_T032_1_01'
+
+--Update comments to confirm data
+UPDATE [dbo].[SurveyQuadrat]
+set [Comments] = CONCAT(Comments, case when Comments is null then 'Data correct' else ', Data correct' end)
+where [QuadratID] like 'TBSRVY_20230321_1_0278_1_05' or [QuadratID] like 'TBSRVY_20230321_1_0277_1_03' or [QuadratID] like 'TBSRVY_20230321_1_0277_1_04' or [QuadratID] like 'TBSRVY_20230321_1_0527_1_05' or [QuadratID] like 'TBSRVY_20230321_1_0528_1_04' or [QuadratID] like 'TBSRVY_20230321_1_0529_1_04' or [QuadratID] like 'TBSRVY_20230627_1_T006_1_01' or [QuadratID] like 'TBSRVY_20230627_1_T006_1_04'  or [QuadratID] like 'TBSRVY_20230627_1_T006_1_05' or [QuadratID] like 'TBSRVY_20230627_1_T007_1_03' or [QuadratID] like 'TBSRVY_20231101_1_T023_1_02' or [QuadratID] like 'TBSRVY_20231101_1_T023_1_03' or [QuadratID] like 'TBSRVY_20231221_1_0527_1_03' or [QuadratID] like 'TBSRVY_20231221_1_0527_1_04' or [QuadratID] like 'TBSRVY_20231227_1_0528_1_02' or [QuadratID] like 'TBSRVY_20231227_1_0529_1_02' or [QuadratID] like 'TBSRVY_20240226_1_T031_1_%' or [QuadratID] like 'TBSRVY_20240326_1_0278_1_04' or [QuadratID] like 'TBSRVY_20240326_1_0527_1_03'
+
+UPDATE [dbo].[SurveySH]
+set [Comments] = CONCAT(Comments, case when Comments is null then 'Data correct' else ', Data correct' end)
+where [ShellHeightID] like 'TBSRVY_20230627_1_T006_1_01_001'
+
+--Update Live count
+UPDATE [dbo].[SurveyQuadrat]
+set NumLive = 7 
+where [QuadratID] like 'TBSRVY_20230822_1_T018_1_03'
+
 
 EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2024-01-01', @CheckEnd = '2024-04-30', @EstuaryCode = 'SL', @DataManager = 'Erica Levine';
 EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2024-01-01', @CheckEnd = '2024-04-30', @EstuaryCode = 'LX', @DataManager = 'Erica Levine';
 EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2024-01-01', @CheckEnd = '2024-04-30', @EstuaryCode = 'LW', @DataManager = 'Erica Levine';
 EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2024-01-01', @CheckEnd = '2024-04-30', @EstuaryCode = 'CR', @DataManager = 'Erica Levine';
 EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2024-01-01', @CheckEnd = '2024-04-30', @EstuaryCode = 'TB', @DataManager = 'Erica Levine';
+EXECUTE [dbo].[spChecksSurvey] @CheckStart = '2023-01-01', @CheckEnd = '2024-05-30', @EstuaryCode = 'TB', @DataManager = 'Erica Levine';
