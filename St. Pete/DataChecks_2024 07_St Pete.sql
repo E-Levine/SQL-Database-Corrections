14590b66afb8f42a982186611ef7b841e4f292c4
E-Levine <149699489+E-Levine@users.noreply.github.com>
2024-07-19 14:30:44 -0400

    Update DataChecks_2024 07_St Pete.sql
    Added code for missing DOPct data.

--- St. Pete/DataChecks_2024 07_St Pete.sql
+++ St. Pete/DataChecks_2024 07_St Pete.sql
@@ -1,11 +1,11 @@
-use [Oysters_24-07-10]
+use [Oysters_24-07-19]
 go
 
 --CAGE checks
 --CR  04-06/2024
 DELETE FROM [dbo].[SampleEventWQ] where [SampleEventWQID] like 'CRCAGE_20240516_1_0231_1_01' and DateEntered = '2024-06-11 00:00:00'
 DELETE FROM [dbo].[SampleEventWQ] where [SampleEventWQID] like 'CRCAGE_20240516_1_0232_1_01' and DateEntered = '2024-06-11 00:00:00'
-UPDATE [dbo].[SampleEventWQ]
+UPDATE [dbo].[SampleEventWQ] 
 set [DataStatus] = 'Proofed',
 	[DateProofed] = '2024-07-10 00:00:00',
 	[ProofedBy] = 'Erica Levine'
@@ -31,6 +31,28 @@ set [DataStatus] = 'Proofed',
 	[DateProofed] = '2024-07-10 00:00:00',
 	[ProofedBy] = 'Erica Levine'
 where [SampleEventWQID] like 'LXCAGE_20240506_1_0243_1_01'
+
+--Add missing DOPct to Column
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 104.0 where [SampleEventWQID] like 'LWCOLL_20240506_1_0235_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 104.1 where [SampleEventWQID] like 'LWCOLL_20240506_1_0236_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 106.3 where [SampleEventWQID] like 'LWCOLL_20240506_1_0237_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 101.9 where [SampleEventWQID] like 'LWCOLL_20240506_1_0240_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 100.8 where [SampleEventWQID] like 'LWCOLL_20240506_1_0241_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 108.0 where [SampleEventWQID] like 'LWCOLL_20240506_1_0312_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 107.8 where [SampleEventWQID] like 'LXCOLL_20240506_1_0242_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 101.0 where [SampleEventWQID] like 'LXCOLL_20240506_1_0243_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 124.5 where [SampleEventWQID] like 'LXCOLL_20240506_1_0246_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 127.9 where [SampleEventWQID] like 'LXCOLL_20240506_1_0247_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 125.0 where [SampleEventWQID] like 'LXCOLL_20240506_1_0249_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 106.8 where [SampleEventWQID] like 'CRCOLL_20240408_1_0230_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 108.0 where [SampleEventWQID] like 'CRCOLL_20240408_1_0232_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 102.8 where [SampleEventWQID] like 'CRCOLL_20240408_1_0233_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 102.1 where [SampleEventWQID] like 'LWCOLL_20240409_1_0237_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 102 where [SampleEventWQID] like 'LWCOLL_20240409_1_0312_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 102.6 where [SampleEventWQID] like 'LXCOLL_20240409_1_0243_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 107.3 where [SampleEventWQID] like 'LXCOLL_20240409_1_0246_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 124.0 where [SampleEventWQID] like 'LXCOLL_20240409_1_0247_1_01'
+UPDATE [dbo].[SampleEventWQ] set PercentDissolvedOxygen = 108.2 where [SampleEventWQID] like 'LXCOLL_20240409_1_0249_1_01'
 
 
 EXECUTE [dbo].[spChecksCage] @CheckStart = '2024-04-01', @CheckEnd = '2024-06-30', @EstuaryCode = 'CR', @DataManager = 'Erica Levine';
