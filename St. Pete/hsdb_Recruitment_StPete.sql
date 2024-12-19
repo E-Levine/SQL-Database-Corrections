21e5415921370c2eeb1c0bdb20f0154f2703ee58
E-Levine <149699489+E-Levine@users.noreply.github.com>
2024-12-19 11:49:27 -0500

    Update hsdb_Recruitment_StPete.sql
    TB RCRT 2008-2011 complete.

--- St. Pete/hsdb_Recruitment_StPete.sql
+++ St. Pete/hsdb_Recruitment_StPete.sql
@@ -1,4 +1,4 @@
---HSDB Checks:
+--HSDB Checks: TB RCRT 2008-2011
 
 --TB RCRT 2008
 --Run TB_RCRT_WQ_EW SQL code First
@@ -62,5 +62,11 @@ UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments
 UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments is null then 'T-bar missing' else ', T-bar missing' end) where SampleEventID like 'TBRCRT_20100512_1_0280%' and ShellReplicate like '3'
 UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments is null then 'T-bars moved' else ', T-bars moved' end) where SampleEventID like 'TBRCRT_20100609_1_0280%'
 UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments is null then 'T-bar missing' else ', T-bar missing' end) where SampleEventID like 'TBRCRT_20100713_1_0280%' and ShellReplicate like '3'
+UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments is null then 'T-bar on ground' else ', T-bar on ground' end) where SampleEventID like 'TBRCRT_20101012_0282%' and ShellReplicate like '3'
 
-EXECUTE [hsdb].[spChecksRecruitment] @CheckStart = '2008-01-01', @CheckEnd = '2010-12-30', @EstuaryCode = 'TB', @DataManager = 'Erica Levine';
+--TB RCRT 2011
+UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments is null then 'Stringers missing' else ', Stringers missing' end) where SampleEventID like 'TBRCRT_20110118_1_0282%'
+UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments is null then 'T-bar on ground' else ', T-bar on ground' end) where SampleEventID like 'TBRCRT_20110815_1_0282%' and ShellReplicate like '1'
+UPDATE [hsdb].[Recruitment] set [Comments] = CONCAT(Comments, case when Comments is null then 'T-bar on ground' else ', T-bar on ground' end) where SampleEventID like 'TBRCRT_20111215_1_0282%' and ShellReplicate like '3'
+
+EXECUTE [hsdb].[spChecksRecruitment] @CheckStart = '2008-01-01', @CheckEnd = '2011-12-30', @EstuaryCode = 'TB', @DataManager = 'Erica Levine';
