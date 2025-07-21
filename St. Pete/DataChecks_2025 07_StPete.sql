44ed4f75d41c19355d951e571fd7349de5ee016e
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-07-21 10:20:05 -0400

    Add SQL script for July 2025 St. Pete data checks
    Introduces a new SQL file for St. Pete July 2025 data checks.
    Update statement for datalogger locations in the Oysters database.

---
+++ St. Pete/DataChecks_2025 07_StPete.sql
@@ -0,0 +1,5 @@
+use [Oysters]
+go
+
+--Update Datalogger locations --COMPLETED in OYSTERS
+--UPDATE FixedLocations set Dataloggers = 'Y' where StationNameNumber = 'TB Braden River 8' or StationNameNumber = 'LW-L-2-2' 
