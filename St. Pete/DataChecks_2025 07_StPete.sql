b617ecd478639c8a418f50b10a1ee2de1770daed
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-07-23 09:03:35 -0400

    July checks
    CAGE: 06/2025; SL/LX 07/2025
    COLL: TB 06/2024, 06/2025; LW/CERP 04-05/2025; SL/LW/LX 07/2025

--- St. Pete/DataChecks_2025 07_StPete.sql
+++ St. Pete/DataChecks_2025 07_StPete.sql
@@ -1,5 +1,34 @@
-use [Oysters_25-07-21]
+use [Oysters_25-07-23]
 go
+
 
 --Update Datalogger locations --COMPLETED in OYSTERS
 --UPDATE FixedLocations set Dataloggers = 'Y' where StationNameNumber = 'TB Braden River 8' or StationNameNumber = 'LW-L-2-2' 
+--LXN sex ratio repro samples for June-July 2025 added to Oysters 7/22/25
+
+--RCRT: TB 06/2025, SL/LX/LW 07/2025
+
+--SDTP: LW 07/2025
+
+--CAGE: 06/2025, SL/LX 07/2025
+UPDATE dbo.CageCount set [Comments] = CONCAT(Comments, ', 29 deployed-miscounted') where CageCountID like 'LXCAGE_20250708_1_0243_1_D_B%'
+
+--COLL: TB 06/2024 Repro. LW 04-05/2025, CERP 04-05/2025, LW/LX/SL dermo 07/2025, TB 06/2025
+UPDATE dbo.ConditionIndex set [Comments] = 'Shallow' where OysterID like 'TBRC2406-04%'
+DELETE from TripInfo where TripID = 'TBCOLL_20250630_1'
+UPDATE dbo.SampleEventWQ set [Secchi] = 0.30 where SampleEventWQID like 'TBCOLL_20250623_1_0529_1_01%'
+
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-06-01', @CheckEnd = '2025-06-30', @EstuaryCode = 'TB', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-07-01', @CheckEnd = '2025-07-30', @EstuaryCode = 'SL', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-07-01', @CheckEnd = '2025-07-30', @EstuaryCode = 'LX', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-07-01', @CheckEnd = '2025-07-30', @EstuaryCode = 'LW', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-07-01', @CheckEnd = '2025-07-30', @EstuaryCode = 'CR', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksSediment] @CheckStart = '2025-07-01', @CheckEnd = '2025-07-30', @EstuaryCode = 'LW', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCage] @CheckStart = '2025-06-01', @CheckEnd = '2025-06-28', @EstuaryCode = 'CR', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCage] @CheckStart = '2025-06-01', @CheckEnd = '2025-07-28', @EstuaryCode = 'SL', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCage] @CheckStart = '2025-06-01', @CheckEnd = '2025-07-28', @EstuaryCode = 'LX', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCollections] @CheckStart = '2024-06-01', @CheckEnd = '2024-06-30', @EstuaryCode = 'TB', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCollections] @CheckStart = '2025-04-01', @CheckEnd = '2025-05-30', @EstuaryCode = 'LW', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCollections] @CheckStart = '2025-01-01', @CheckEnd = '2025-05-30', @EstuaryCode = 'LX', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCollections] @CheckStart = '2025-04-01', @CheckEnd = '2025-05-30', @EstuaryCode = 'SL', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksCollections] @CheckStart = '2025-04-01', @CheckEnd = '2025-05-30', @EstuaryCode = 'CR', @DataManager = 'Erica Williams';
