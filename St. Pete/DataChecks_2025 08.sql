3fed32cccdb17f546613eb4c90d1628f2b69ad8b
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-08-25 08:41:47 -0400

    Add August 2025 St. Pete data check SQL script
    New script for SP Aug checks.

---
+++ St. Pete/DataChecks_2025 08.sql
@@ -0,0 +1,12 @@
+--St Pete corrections
+--August 2025
+use Oysters
+go
+
+--Update CR dermo: -- completed in Oysters
+--INSERT INTO [dbo].[Dermo] ([OysterID],[SampleEventID],[ShellHeight],[ShellLength],[ShellWidth],[TotalWeight],[ShellWetWeight],[DermoMantle],[DermoGill],[DataStatus],[DateEntered],[EnteredBy],[DateProofed],[ProofedBy],[DateCompleted],[CompletedBy],[Comments],[AdminNotes])
+ --VALUES ('CRED2507-01','CRCOLL_20250724_1_0230_1',79.33,NULL,NULL,44.74,34.42,0.5,0,'Proofed','2025-08-25','Erica Williams','2025-08-25','Erica Williams',NULL,NULL,'0','Added via SMSS')
+ -- GO
+--INSERT INTO [dbo].[Dermo] ([OysterID],[SampleEventID],[ShellHeight],[ShellLength],[ShellWidth],[TotalWeight],[ShellWetWeight],[DermoMantle],[DermoGill],[DataStatus],[DateEntered],[EnteredBy],[DateProofed],[ProofedBy],[DateCompleted],[CompletedBy],[Comments],[AdminNotes])
+ -- VALUES ('CRED2507-02','CRCOLL_20250724_1_0230_1',53,NULL,NULL,22.97,15.93,0.5,0,'Proofed','2025-08-25','Erica Williams','2025-08-25','Erica Williams',NULL,NULL,'0','Added via SMSS')
+ --GO
