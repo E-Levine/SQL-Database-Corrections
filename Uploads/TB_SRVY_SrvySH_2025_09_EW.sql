dbdbbe9decd315c54b26552db8d584af13057ddc
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-10-23 08:58:10 -0400

    Data uploads
    data files

---
+++ Uploads/TB_SRVY_SrvySH_2025_09_EW.sql
@@ -0,0 +1,301 @@
+use [Oysters_25-10-23]
+go
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 36,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_001'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 50,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_002'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 65,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_003'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 53,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_004'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 70,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_005'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 15,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_006'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 58,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_007'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 11,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_008'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 20,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_009'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 29,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_010'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 22,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_011'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 42,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_012'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 55,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_013'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 39,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_014'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 22,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_015'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 34,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_016'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 45,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_017'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 36,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_018'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 43,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_019'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 27,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_020'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 21,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_021'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 13,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_022'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 76,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_023'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 18,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_024'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 37,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_025'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 38,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_026'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 8,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_027'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 43,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_028'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 48,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_029'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 42,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_030'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 45,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_031'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 34,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_032'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 39,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_033'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 14,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_034'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 28,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_035'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 50,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_036'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 10,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_037'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 30,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_038'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 17,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_039'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 33,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_040'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 46,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_041'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 54,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_042'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 65,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_043'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 25,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_044'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 16,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_045'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 10,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_046'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 20,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_047'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 12,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_048'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 30,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_049'
+
+
+UPDATE [dbo].[SurveySH]
+SET [ShellHeight] = 54,
+Comments = 'Updated via SMSS'
+WHERE [ShellHeightID] = 'TBSRVY_20250924_1_0527_1_02_050'
