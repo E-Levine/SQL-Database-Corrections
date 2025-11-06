4257a3ebf324afc4ff066a5c246716c9e1e13a26
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-11-06 07:43:44 -0500

    Update DataChecks_2025 11_SrPete.sql

--- St. Pete/DataChecks_2025 11_SrPete.sql
+++ St. Pete/DataChecks_2025 11_SrPete.sql
@@ -3,3 +3,6 @@ go
 
 --Cages moved SLC1 > SLC2 -- COMPLETED ON OYSTERS
 --UPDATE FixedLocations set Cage = 'Y' where FixedLocationID = '0256'
+
+--Extra sample:
+DELETE from hsdb.Dermo where OysterID like 'LXSD2112-01' and SampleEventID like 'LXCOLL_20211207_1_0249_1'
