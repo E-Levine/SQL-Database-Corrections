6cddfdbc4b767c6ff8248f2a9a0289054aef7314
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-11-24 08:15:54 -0500

    St Pete updates
    November 2025

--- St. Pete/DataChecks_2025 11_StPete.sql
+++ St. Pete/DataChecks_2025 11_StPete.sql
@@ -1,4 +1,4 @@
-use [Oysters]
+use [Oysters_25-11-24]
 go
 
 --Cages moved SLC1 > SLC2 -- COMPLETED ON OYSTERS
@@ -7,4 +7,19 @@ go
 --Extra sample:
 DELETE from hsdb.Dermo where OysterID like 'LXSD2112-01' and SampleEventID like 'LXCOLL_20211207_1_0249_1'
 
---Example comment.
+-- LW SDTP 11/2025. Good.
+
+--CERP, PBC dermo checks 11/2025
+--Add comment to Sex ratio samples, check SLS2511-10 data
+UPDATE [dbo].[Dermo] set Comments = CONCAT('Sex ratio project sample ', Comments) where OysterID = 'LXND2511-16' or OysterID = 'LXND2511-17' or OysterID = 'LXND2511-18' or 
+	OysterID = 'LXND2511-19' or OysterID like 'LXND2511-2%' or OysterID like 'LXND2511-3%' or OysterID like 'LXND2511-4%' 
+
+--CERP, PBC RCRT checks 11/2025
+--Update CRW4 deployed date
+Update [dbo].[Recruitment] set DeployedDate = '2025-10-14' where SampleEventID = 'CRRCRT_20251112_1_0233_1'
+
+EXECUTE [dbo].[spChecksSediment] @CheckStart = '2025-11-01', @CheckEnd = '2025-11-30', @EstuaryCode = 'LW', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-11-01', @CheckEnd = '2025-11-30', @EstuaryCode = 'SL', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-11-01', @CheckEnd = '2025-11-30', @EstuaryCode = 'LX', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-11-01', @CheckEnd = '2025-11-30', @EstuaryCode = 'LW', @DataManager = 'Erica Williams';
+EXECUTE [dbo].[spChecksRecruitment] @CheckStart = '2025-11-01', @CheckEnd = '2025-11-30', @EstuaryCode = 'CR', @DataManager = 'Erica Williams';
